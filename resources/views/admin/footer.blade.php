ed7f335909c7383b3fb8b53c3f38c0ef4b984a72
Avetis Azatyan <avetis.azatyan17@gmail.com>
2022-11-17 23:00:08 +0400

    finished implementation template

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('dist/js/jquery.min.js') }}"></script>
+<script src="{{ asset("dist/js/bootstrap.bundle.min.js") }}"></script>
+<script src="{{ asset("dist/js/adminlte.js") }}"></script>
+<script src="{{ asset("dist/js/Chart.min.js") }}"></script>
+<script src="{{ asset("dist/js/demo.js") }}"></script>
+<script src="{{ asset("dist/js/dashboard3.js") }}"></script>
+
+</body>
+</html>
