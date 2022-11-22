40502e1216835545ce21550653106e20eac5a4ed
Avetis Azatyan <avetis.azatyan17@gmail.com>
2022-11-22 23:05:03 +0400

    Crete blade files

--- resources/views/admin/head.blade.php
+++ resources/views/admin/head.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRM For Library</title>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset("dist/css/all.min.css") }}">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
